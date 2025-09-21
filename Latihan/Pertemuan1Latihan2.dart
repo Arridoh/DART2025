ed48d2e14291eb3067510ac1dc08a3a0e3fcb2f5
Arridoh Fuad <arridohfuad@gmail.com>
2025-09-22 07:06:13 +0900

    Tambahkan Folder untuk latihan.

---
+++ Latihan/Pertemuan1Latihan2.dart
@@ -0,0 +1,11 @@
+void main() {
+  double panjang = 7;
+  double lebar = 7;
+  double luas = panjang * lebar;
+  double keliling = 2 * (panjang + lebar);
+
+  print("Panjang = $panjang");
+  print("Lebar = $lebar");
+  print("Luas = $luas");
+  print("Keliling = $keliling");
+}
